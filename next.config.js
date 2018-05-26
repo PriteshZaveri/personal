2455b132cbc52185af504d9ae30ac60bac3a4004
Pritesh Zaveri <priteshjhaveri27@gmail.com>
2018-05-26 13:44:03 +0530

    Procfile

--- next.config.js
+++ next.config.js
@@ -2,10 +2,16 @@
 const withCSS = require("@zeit/next-css");
 module.exports = withCSS({
   webpack(config, options) {
-    config.module.rules.push({
-      test: /\.(png|jpg)$/,
-      loader: "url-loader"
-    });
+    config.module.rules.push(
+      {
+        test: /\.(png|jpg)$/,
+        loader: "url-loader"
+      },
+      {
+        test: /\.(ttf|otf)$/,
+        loader: "file-loader"
+      }
+    );
     return config;
   }
 });
